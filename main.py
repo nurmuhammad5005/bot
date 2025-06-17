0cc21e3685f8304a45687dbd495c95723e57628c
Nurmuhammad <nurmuhammadfazlillaev@gmail.com>
2025-06-17 15:47:47 +0300

    test downloading

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 import aiogram 
-import os 
+import os
+
+import aiogram.filters 
 import dotenv
 import asyncio
+import yt_dlp
 
 dotenv.load_dotenv()
 
 bot = aiogram.Bot(os.getenv("BOT_TOKEN"))
 dp = aiogram.Dispatcher()
 
+@dp.message(aiogram.filters.Command("start"))
+def start_handler(message: aiogram.types.Message):
+    url = "https://youtube.com/shorts/5bjOrfkdNo8?si=7zDc2FvdUAlO3Wje"
+    opts = {"format": "best", "outtmpl": "video.%(ext)s"}
+
+    with yt_dlp.YoutubeDL(opts) as ydl:
+        info = ydl.download([url])
+        print(info)
 
 def on_start():
     print("bot has been started...")
